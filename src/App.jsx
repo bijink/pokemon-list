9679dcd23f2a5ff1f4db75002a64cb71eef2fe55
bijink <kbijin528.pro@gmail.com>
2023-03-15 20:49:40 +0530

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import "./App.css";
 import Details from "./components/details/Details";
+import Header from "./components/header/Header";
 import List from "./components/list/List";
 
 export const PokemonDetails = createContext();
@@ -10,12 +11,15 @@ function App() {
 
   return (
     <>
-      <PokemonDetails.Provider value={{ pokemonDetails, setPokemonDetails }}>
-        <div className="App">
-          <List />
-          <Details />
-        </div>
-      </PokemonDetails.Provider>
+      <div className="app">
+        <Header />
+        <PokemonDetails.Provider value={{ pokemonDetails, setPokemonDetails }}>
+          <div className="app__main">
+            <List />
+            <Details />
+          </div>
+        </PokemonDetails.Provider>
+      </div>
     </>
   );
 }
